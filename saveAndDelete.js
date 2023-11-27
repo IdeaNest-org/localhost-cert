3f4cacbac134a8d01f3ff0652bc808973018dbff
roc.liu@hotmail.com <roc.liu@hotmail.com>
2023-11-27 18:09:32 +0800

    update

--- saveAndDelete.js
+++ saveAndDelete.js
@@ -14,53 +14,64 @@ const octokit = new Octokit({
     auth: getGitHubToken(), // 请替换为你的个人访问令牌
 });
 
-async function uploadFileToGitHubRepo(owner, repo, filePath, branch, message) {
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const contentEncoded = Buffer.from(fileContent).toString('base64');
+module.exports = async function saveAndDelete() {
+    // 保存证书到本地
+    async function uploadFileToGitHubRepo(
+        owner,
+        repo,
+        filePath,
+        branch,
+        message
+    ) {
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        const contentEncoded = Buffer.from(fileContent).toString('base64');
 
-    try {
-        const response = await octokit.repos.createOrUpdateFileContents({
-            owner: owner,
-            repo: repo,
-            path: filePath,
-            message: message,
-            content: contentEncoded,
-            branch: branch,
-        });
-        console.log('File uploaded to GitHub:', response.data.content.html_url);
-    } catch (error) {
-        console.error('Error uploading file to GitHub:', error.message);
+        try {
+            const response = await octokit.repos.createOrUpdateFileContents({
+                owner: owner,
+                repo: repo,
+                path: filePath,
+                message: message,
+                content: contentEncoded,
+                branch: branch,
+            });
+            console.log(
+                'File uploaded to GitHub:',
+                response.data.content.html_url
+            );
+        } catch (error) {
+            console.error('Error uploading file to GitHub:', error.message);
+        }
     }
-}
-const rootCrt = fs.readFileSync('root.crt', 'utf-8');
-const localhostCrt = fs.readFileSync('localhost.crt', 'utf-8');
-const localhostKey = fs.readFileSync('localhost.key', 'utf-8');
-console.log(rootCrt);
-console.log(localhostCrt);
-console.log(localhostKey);
+    const rootCrt = fs.readFileSync('root.crt', 'utf-8');
+    const localhostCrt = fs.readFileSync('localhost.crt', 'utf-8');
+    const localhostKey = fs.readFileSync('localhost.key', 'utf-8');
+    console.log(rootCrt);
+    console.log(localhostCrt);
+    console.log(localhostKey);
 
-fs.unlinkSync('root.key');
-// 使用示例
-uploadFileToGitHubRepo(
-    'IdeaNest-org',
-    'localhost-cert',
-    'root.crt',
-    'main',
-    fs.readFileSync('root.crt', 'utf-8')
-);
+    // 使用示例
+    uploadFileToGitHubRepo(
+        'IdeaNest-org',
+        'localhost-cert',
+        'root.crt',
+        'main',
+        fs.readFileSync('root.crt', 'utf-8')
+    );
 
-uploadFileToGitHubRepo(
-    'IdeaNest-org',
-    'localhost-cert',
-    'localhost.crt',
-    'main',
-    fs.readFileSync('localhost.crt', 'utf-8')
-);
+    uploadFileToGitHubRepo(
+        'IdeaNest-org',
+        'localhost-cert',
+        'localhost.crt',
+        'main',
+        fs.readFileSync('localhost.crt', 'utf-8')
+    );
 
-uploadFileToGitHubRepo(
-    'IdeaNest-org',
-    'localhost-cert',
-    'localhost.key',
-    'main',
-    fs.readFileSync('localhost.key', 'utf-8')
-);
+    uploadFileToGitHubRepo(
+        'IdeaNest-org',
+        'localhost-cert',
+        'localhost.key',
+        'main',
+        fs.readFileSync('localhost.key', 'utf-8')
+    );
+};

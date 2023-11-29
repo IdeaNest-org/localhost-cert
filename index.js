8749d57ae6b5d23a4c80500c2fcc825427ab02f9
pengzai <xx@xx.com>
2023-11-29 23:06:16 +0800

    update autoInstall default value to true

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function readFileFromDirAsync(file) {
 
 // 调用检测函数
 
-module.exports = getHttps = ({ autoInstall = false } = {}) => {
+module.exports = getHttps = ({ autoInstall = true } = {}) => {
     if (autoInstall) {
         checkRootCertificateInstallation().then((res) => {
             if (!res) {
